1ef1c95f5a6014fbd8be5f6c0d1119aa776cee0c
Leila Rhazaoui <lelarhaz@gmail.com>
2018-04-03 16:58:52 +0200

    sorted blocks by position number and added block colors

--- client/js/components/block.js
+++ client/js/components/block.js
@@ -1,5 +1,5 @@
 export const createBlockElement = block => `
-  <a class="block" href="${block.url}">
+  <a class="block" href="${block.url}" target="_blank"  style="background-color: ${block.color};">
     <div class="block-content">
       <p>${block.title}</p>
       <p>LOGO</p>
