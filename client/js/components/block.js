227eb37d3f8ca8d30614c5f7788d15e5d7966056
Marie Malarme <malarmemarie@gmail.com>
2018-04-04 10:10:33 +0200

    added colors and size of the blocks in CSS, edited color codes in blocks.json

--- client/js/components/block.js
+++ client/js/components/block.js
@@ -1,8 +1,8 @@
-export const createBlockElement = block => `
-  <a class="block" href="${block.url}" target="_blank"  style="background-color: ${block.color};">
-    <div class="block-content">
-      <p>${block.title}</p>
-      <p>LOGO</p>
-    </div>
-  </a>
-`
+export const createBlockElement = block =>
+	`<a class="block" href="${block.url}" target="_blank" style="background-color: ${block.color}">
+		<div class="block-content">
+			<p>${block.title}</p>
+			<p>LOGO</p>
+		</div>
+	</a>`
+	
