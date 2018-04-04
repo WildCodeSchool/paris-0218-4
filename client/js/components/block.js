e9ede100bc570ec2c177c62e1af15d55a46d263c
Marie Malarme <malarmemarie@gmail.com>
2018-04-04 14:24:23 +0200

    optimize the layout of the blocks content

--- client/js/components/block.js
+++ client/js/components/block.js
@@ -1,8 +1,10 @@
 export const createBlockElement = block =>
 	`<a class="block" href="${block.url}" target="_blank" style="background-color: ${block.color}">
-		<div class="block-content">
+		<div class="block-icon">
+			<img src="${block.icon}"></p>
+		</div>
+		<div class="block-title">
 			<p>${block.title}</p>
-			<p>LOGO</p>
 		</div>
 	</a>`
 	
