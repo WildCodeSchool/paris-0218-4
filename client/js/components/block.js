3976be0c73ea01896427f9d77247b7d3654d546c
Marie Malarme <malarmemarie@gmail.com>
2018-04-04 15:34:44 +0200

    test : integration of icons in the blocks

--- client/js/components/block.js
+++ client/js/components/block.js
@@ -1,10 +1,10 @@
 export const createBlockElement = block =>
 	`<a class="block" href="${block.url}" target="_blank" style="background-color: ${block.color}">
 		<div class="block-icon">
-			<img src="${block.icon}"></p>
+			<img class="icon" src="${block.icon}"></p>
 		</div>
 		<div class="block-title">
-			<p>${block.title}</p>
+			<p style="color: ${block.titleColor}">${block.title}</p>
 		</div>
 	</a>`
 	
