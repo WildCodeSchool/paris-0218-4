90a337b344ff3967360429852a634d4b98a1e77d
Marie Malarme <malarmemarie@gmail.com>
2018-04-05 11:44:38 +0200

    otpimization of media queries

--- client/js/components/block.js
+++ client/js/components/block.js
@@ -2,13 +2,18 @@ export const createBlockElement = block =>
 	`<div class="block" style="background-color:${block.color}">
 		<div class="edit-delete">
 			<p class="edit-button"><a href="" style="color:${block.titleColor}; text-decoration:none">Edit</a></p>
-			<span style="color:${block.titleColor}; text-decoration:none">|</span> 
+			<span class="separator" style="color:${block.titleColor}; text-decoration:none">|</span> 
 			<p class="delete-button"><a href="" style="color:${block.titleColor}; text-decoration:none">Delete</a></p>
 		</div>
-		<a href="${block.url}" target="_blank"><div class="block-icon">
-			<img class="icon" src="${block.icon}"></p>
-		</div></a>
-		<a href="${block.url}" target="_blank" style="text-decoration:none"><div class="block-title">
-			<p style="color: ${block.titleColor}">${block.title}</p>
-		</div>
+
+		<a href="${block.url}" target="_blank">
+			<div class="block-icon">
+				<img class="icon" src="${block.icon}"></p>
+			</div>
+		</a>
+		<a href="${block.url}" target="_blank" style="text-decoration:none">
+			<div class="block-title">
+				<p style="color: ${block.titleColor}">${block.title}</p>
+			</div>
+		</a>
 	</div>`
