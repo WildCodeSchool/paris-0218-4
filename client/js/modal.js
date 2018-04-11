9428329bf952fb3fa668e37f8f4bc2f3f59d0b4d
shvan7 <shvann.7@gmail.com>
2018-04-11 16:21:38 +0200

    add style of form module

--- client/js/modal.js
+++ client/js/modal.js
@@ -1,23 +1,31 @@
-const user_agent =/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-// Get the modal
-const modal = document.getElementById('myModal');
-const btn = document.getElementById("open_form");
-const close_btn = document.getElementById("close_modal");
-const modalContent = document.getElementById('modal-content')
+const modal = document.getElementById('myModal')
 
-// When the user clicks on the button, open the modal
-btn.onclick = () => {
+const displayModal = () => {
+  const user_agent =/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+  const modalContent = document.getElementById('modal-content')
   modal.style.display = "block"
+
+  // portrait
   if (user_agent && (window.innerHeight > window.innerWidth) && (modal.style.display === "block")) {
-    modalContent.style.width = '95%'
+    modalContent.style.width = '100%'
+    modalContent.style.marginTop = '0%'
+    modal.style.overflow = 'hidden'
+  }
+  // payasage
+  else if (user_agent && (window.innerHeight < window.innerWidth) && (modal.style.display === "block")) {
+    modalContent.style.marginTop = '1%'
+    modalContent.style.width = '70%'
+    modal.style.overflow = 'scroll'
   }
   else {
-    modalContent.style.marginTop = '5%'
-    modalContent.style.width = '70%'
+    // on pc
+    modalContent.style.marginTop = '15%'
+    modalContent.style.width = '400px'
   }
 }
+
 // When the user clicks on <span> (x), close the modal
-close_btn.onclick = () => {
+document.getElementById("close_modal").onclick = () => {
   modal.style.display = "none"
 }
 // When the user clicks anywhere outside of the modal, close it
@@ -26,21 +34,12 @@ window.onclick = (event) => {
     modal.style.display = "none"
   }
 }
-// IF MOBILE
-window.addEventListener("DOMContentLoaded", () => {
-  // if mobile and portrait
-  if (user_agent && (window.innerHeight > window.innerWidth) && (modal.style.display === "block")) {
-    modalContent.style.width = '95%'
-  }
-})
+
+// When the user clicks on the button, open the modal
+document.getElementById("open_form").onclick = () => {
+  displayModal()
+}
 // IF MOBILE RESIZE portrait/paysage
 window.addEventListener("resize", () => {
-  if (window.innerHeight > window.innerWidth) {
-    modalContent.style.width = '95%'
-    modalContent.style.marginTop = '15%'
-  }
-  else {
-    modalContent.style.marginTop = '5%'
-    modalContent.style.width = '70%'
-  }
+  displayModal()
 }, false)
