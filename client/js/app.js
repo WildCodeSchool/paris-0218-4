176c9c3ea7a143a7c9b113fab8199dec61c23d92
3c2h <Alexccx@Laposte.net>
2018-04-17 21:44:21 +0200

    trying to make things work...

--- client/js/app.js
+++ client/js/app.js
@@ -21,3 +21,21 @@ fetch('http://localhost:3030/blocks')
 
     setup()
 })
+
+document.getElementById('addBlock').addEventListener('submit', event => {
+  event.preventDefault()
+  const title = document.getElementById('title').value
+  const url = document.getElementById('url').value
+  console.log('HAHAHAHAHA')
+  console.log(title)
+  console.log(url)
+
+  fetch('http://localhost:3030/blocks', {
+    method: 'post',
+    body: JSON.stringify({
+      title,
+      url
+    })
+  }).then(res => console.log(res.status))
+})
+
--- server/index.js
+++ server/index.js
@@ -1,20 +1,69 @@
 const express = require('express')
-
+const fs = require('fs')
+const util = require('util')                //  Filesystem
+const path = require('path')            //  Util. pour les chemins d'accès
 const port = 3030
-
 const blocks = require('../mocks/blocks/blocks.json')
-
 const app = express()
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
 
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*')
   response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
-app.get('/', (request, response) => {
-  response.send('OK')
+// app.get('/', (request, response) => {
+//   response.send('OK')
+// })
+
+app.use((request, response, next) => {
+  if (request.method === 'GET') return next()
+  let accumulator = ''
+
+  request.on('data', data => {
+    accumulator += data
+  })
+
+  request.on('end', () => {
+    try {
+      request.body = JSON.parse(accumulator)
+      next()
+    } catch (err) {
+      next(err)
+    }
+  })
 })
-app.get('/blocks', (request, response) =>{
+
+app.listen(port, () => console.log(`server listenning to port ${port}`))
+
+//_________________________________________//
+            //  FILE SYSTEM //
+
+const filePath = path.join(__dirname, '../mocks/blocks/blocks.json')
+
+app.get('/', (request, response) => {
+  response.send('OKokayyyyyyyyye')
+    response.json(JSON.parse(data))
+} )
+
+app.get('/blocks', (request, response) => {
   response.json(blocks)
 })
-app.listen(port, () => console.log(`server listenning to port ${port}`))
+
+// return response.status(404).end('Not found')
+
+app.post('/blocks', (request, response, next) => {
+  const filepath = '../mocks/blocks/blocks.json'
+
+  const content = {
+    title: request.body.title,
+    url: request.body.url
+  }
+
+  writeFile(filepath, JSON.stringify(content), 'utf8')
+    .then(() => response.json('OK'))
+    .catch(next)
+})
+
