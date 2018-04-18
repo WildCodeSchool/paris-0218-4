5e43ca3a99753d18b9f2ab5de3cdca28d6148f0f
shvan7 <shvann.7@gmail.com>
2018-04-18 18:10:27 +0200

    clear code of js client file, rename id of form and keep form modal open after submit

--- client/js/app.js
+++ client/js/app.js
@@ -1,5 +1,9 @@
 import { createBlockElement, createPlusBlockElement } from './components/block.js'
-import { displayModal } from './modal.js'
+import { displayModal, hideModal } from './modal.js'
+
+const blocksContainer = document.getElementById('blocks')
+const formElement = document.getElementById('new-module-form')
+const formSubmitButtonElement = document.getElementById('new-module-form-submit-button')
 
 // display modal
 const setup = () => {
@@ -7,57 +11,69 @@ const setup = () => {
   plusBlock.addEventListener('click', displayModal)
 }
 
-const getData = () => {
-  fetch('http://localhost:3030/blocks')
-  .then(response => response.json())
-  .then(blocks => {
-    const blocksContainer = document.getElementById('blocks')
-    const plusBlockElement = createPlusBlockElement()
-    const blockElements = blocks
+const render = blocks => {
+  const plusBlockElement = createPlusBlockElement()
+  let blockElements = blocks
     .sort((block1, block2) =>  block1.position - block2.position)
     .map(createBlockElement)
     .concat([ plusBlockElement ])
     .join('')
 
-    blocksContainer.innerHTML = blockElements
-    setup()
-  })
+  blocksContainer.innerHTML = blockElements
+
+  setup()
+}
+
+const getModules = () => {
+  return fetch('http://localhost:3030/blocks')
+    .then(response => response.json())
 }
 
 // after DOM load
 window.addEventListener("DOMContentLoaded", () => {
-  getData()
+  getModules().then(render)
 })
 
-document.getElementById('btnSubmit').addEventListener('click', () => {
-  document.getElementById('modalForm').style.display = 'none'
-})
-
-// Post DATA
-document.getElementById('addBlock').addEventListener('submit', event => {
-  event.preventDefault()
-  const body = {
-    title: document.getElementById('title').value,
-    url: document.getElementById('url').value,
-    icon: document.getElementById('selectIcon').value,
-    color: document.getElementById('selectColor').value
-  }
-  document.getElementById('addBlock').reset()
-  fetch('http://localhost:3030/blocks', {
+const sendNewModule = module => {
+  return fetch('http://localhost:3030/blocks', {
     method: 'post',
-    body: JSON.stringify(body)
+    body: JSON.stringify(module)
   })
   .then(res => res.json())
-  .then(blocks => {
-    const blocksContainer = document.getElementById('blocks')
-    const plusBlockElement = createPlusBlockElement()
-    const blockElements = blocks
-    .sort((block1, block2) =>  block1.position - block2.position)
-    .map(createBlockElement)
-    .concat([ plusBlockElement ])
-    .join('')
+}
 
-    blocksContainer.innerHTML = blockElements
-    setup()
+
+const handleSuccess = () => {
+  const color = formSubmitButtonElement.style.backgroundColor
+
+  formSubmitButtonElement.style.backgroundColor = '#8bc34a' // green
+
+  setTimeout(() => {
+    formSubmitButtonElement.style.backgroundColor = color
+    formSubmitButtonElement.disabled = true
+  }, 300)
+}
+
+const handleFailure = err => { console.log(err) }
+
+const handleSubmit = event => {
+  event.preventDefault()
+
+  const module = {
+    title: document.getElementById('new-module-form-title').value,
+    url: document.getElementById('new-module-form-url').value,
+    icon: document.getElementById('new-module-form-icon').value,
+    color: document.getElementById('new-module-form-color').value
+  }
+
+  sendNewModule(module)
+  .then(blocks => {
+    formElement.reset()
+    // hideModal()
+    handleSuccess()
+    render(blocks)
   })
-})
+  .catch(handleFailure)
+}
+
+formElement.addEventListener('submit', handleSubmit)
--- client/js/processingModal.js
+++ client/js/processingModal.js
@@ -1,9 +1,9 @@
 const check = () => {
-  const title = document.getElementById("title").validity.valid
-  const url = document.getElementById("url").validity.valid
-  const icon = document.getElementById("selectIcon").value
-  const color = document.getElementById("selectColor").value
-  const btnSubmit = document.getElementById('btnSubmit')
+  const title = document.getElementById('new-module-form-title').validity.valid
+  const url = document.getElementById('new-module-form-url').validity.valid
+  const icon = document.getElementById('new-module-form-icon').value
+  const color = document.getElementById('new-module-form-color').value
+  const btnSubmit = document.getElementById('new-module-form-submit-button')
   btnSubmit.disabled = icon && color && (title + url === 2) ? false : true
 }
 const resetSelectIcon = () => {
@@ -19,7 +19,7 @@ const resetSelectColor = () => {
 
 Array.from(document.getElementsByClassName('btn-select-icon')).forEach(e => {
   e.addEventListener('click', (elemt) => {
-    document.getElementById('selectIcon').value = elemt.currentTarget.name
+    document.getElementById('new-module-form-icon').value = elemt.currentTarget.name
     resetSelectIcon()
     check()
     e.style.opacity = '1'
@@ -28,14 +28,14 @@ Array.from(document.getElementsByClassName('btn-select-icon')).forEach(e => {
 
 Array.from(document.getElementsByClassName('btn-select-color')).forEach(e => {
   e.addEventListener('click', (elemt) => {
-    document.getElementById('selectColor').value = elemt.currentTarget.name
+    document.getElementById('new-module-form-color').value = elemt.currentTarget.name
     resetSelectColor()
     check()
     e.children[0].style.border = '3px solid #b3b3b3'
   })
 })
 
-const allInput = document.getElementById('addBlock').getElementsByTagName("input")
+const allInput = document.getElementById('new-module-form').getElementsByTagName('input')
 Array.from(allInput).forEach(elemt => {
   elemt.addEventListener('input', evt => {
     check()
