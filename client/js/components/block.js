3d65026a1fa356516a7a0aec51e4bd805c5f1e28
Yoann Cribier <ycribier@student.42.fr>
2018-04-20 12:13:42 +0200

    clean indentation

--- client/js/components/block.js
+++ client/js/components/block.js
@@ -1,35 +1,30 @@
-const linkDeleteModif = (block) => {
-	if (document.isAdmin) {
-		return `
-		<div class="edit-delete">
-		<p class="edit-button"><a href="" style="color:${block.titleColor}; text-decoration:none">Edit</a></p>
-		<span class="separator" style="color:${block.titleColor}; text-decoration:none">|</span>
-		<p class="delete-button"><a href="" style="color:${block.titleColor}; text-decoration:none">Delete</a></p>
-		</div>`
-	} else {
-		return ""
-	}
-}
+const createAdminEditionHeader = block => `
+  <div class="edit-delete">
+    <p class="edit-button"><a href="" style="color:${block.titleColor}; text-decoration:none">Edit</a></p>
+    <span class="separator" style="color:${block.titleColor}; text-decoration:none">|</span>
+    <p class="delete-button"><a href="" style="color:${block.titleColor}; text-decoration:none">Delete</a></p>
+  </div>
+`
 
 export const createBlockElement = block => `
 	<div class="block" style="background-color:${block.color}">
-	${linkDeleteModif(block)}
-	<a href="${block.url}" target="_blank">
-	<div class="block-icon">
-	<img class="icon" src="${block.icon}"></p>
+	  ${document.isAdmin ? createAdminEditionHeader(block) : ''}
+    <a href="${block.url}" target="_blank">
+      <div class="block-icon">
+        <img class="icon" src="${block.icon}"></p>
+      </div>
+    </a>
+    <a href="${block.url}" target="_blank" style="text-decoration:none">
+      <div class="block-title">
+        <p style="color: ${block.titleColor}">${block.title}</p>
+      </div>
+    </a>
 	</div>
-	</a>
-	<a href="${block.url}" target="_blank" style="text-decoration:none">
-	<div class="block-title">
-	<p style="color: ${block.titleColor}">${block.title}</p>
-	</div>
-	</a>
-	</div>
-	`
+`
 
-	export const createPlusBlockElement = () => `
+export const createPlusBlockElement = () => `
 	<div id="block-plus" class="block">
-	<p class="plus">+</p>
-	<p class="add-new-module">Add a new module</p>
+	  <p class="plus">+</p>
+	  <p class="add-new-module">Add a new module</p>
 	</div>
-	`
+`
