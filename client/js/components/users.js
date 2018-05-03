e9d14e9540225073e964e1fc6cd2eec98e84a0ea
Marie Malarme <malarmemarie@gmail.com>
2018-05-03 12:36:34 +0200

    last changes for the visual styles, mainly CSS

--- client/js/components/users.js
+++ client/js/components/users.js
@@ -2,6 +2,6 @@ export const createBlocksUsers = users => `
 <tr>
     <th class="name-user">${users.username}</th>
     <td class="status-user" style="color:${users.color}">${users.admin}</td>
-    <td><button type="button" class="delete-user" name="btn-${users.id}"></button></td>
+    <td class="btn-delete-user"><button type="button" class="delete-user" name="btn-${users.id}"></button></td>
 </tr>
 `
